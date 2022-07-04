2194fdeefad0b38bbfaaa7c56b8289044ded6f7c
Asi Meir <45923568+prayingmantislab@users.noreply.github.com>
2022-07-04 15:06:47 +0300

    added red circle

--- src/Chawazi.tsx
+++ src/Chawazi.tsx
@@ -1,16 +1,53 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Text, View } from 'react-native';
+import React, { useRef } from 'react';
+import { View, StyleSheet, PanResponder, Animated } from 'react-native';
 // @ts-ignore
-const Chawazi = ({ navigation, route }) => {
+const Chawazi = () => {
+  const pan = useRef(new Animated.ValueXY()).current;
+  // @ts-ignore
+
+  const panResponder = useRef(
+    // @ts-ignore
+
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        pan.setOffset({
+          // @ts-ignore
+
+          x: pan.x._value,
+          // @ts-ignore
+
+          y: pan.y._value,
+        });
+      },
+      onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }]),
+      onPanResponderRelease: () => {
+        pan.flattenOffset();
+      },
+    })
+  ).current;
   return (
     <>
       <View>
-        <Text>CHAWAZI</Text>
+        <Animated.View
+          style={{
+            transform: [{ translateX: pan.x }, { translateY: pan.y }],
+          }}
+          {...panResponder.panHandlers}
+        >
+          <View style={styles.box} />
+        </Animated.View>
       </View>
     </>
   );
 };
-
+// @ts-ignore
+const styles = StyleSheet.create({
+  box: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'red',
+    borderRadius: 25,
+  },
+});
 export default Chawazi;
---
+++ src/components/tapView.ts
@@ -0,0 +1,13 @@
+// import React, { useRef } from 'react';
+// import { View, Text, PanResponder } from 'react-native';
+
+// function TapView(): any {
+//   /*@ts-ignore  */
+//   return (
+//     <View>
+//       <Text>TapView</Text>
+//     </View>
+//   );
+// }
+
+// export default TapView;
